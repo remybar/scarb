8b249d18f8b9f5e36d9de48cb11b4a287ac4e08d
glihm <dev@glihm.net>
2025-04-15 16:59:10 -0600

    feat: apply dojo patch

--- scarb/src/compiler/plugin/mod.rs
+++ scarb/src/compiler/plugin/mod.rs
@@ -11,6 +11,8 @@ use serde::{Deserialize, Serialize};
 use crate::compiler::plugin::builtin::BuiltinCairoRunPlugin;
 use crate::compiler::plugin::proc_macro::compilation::SharedLibraryProvider;
 use crate::core::{Package, PackageId, TargetKind, Workspace};
+// Dojo is added as std until proc macro are fully supported, to remove after proc macro are fully supported.
+use crate::core::{PackageName, SourceId};
 
 use self::builtin::{BuiltinStarknetPlugin, BuiltinTestPlugin};
 
@@ -70,6 +72,7 @@ impl CairoPluginRepository {
         repo.add(Box::new(BuiltinTestPlugin)).unwrap();
         repo.add(Box::new(BuiltinCairoRunPlugin)).unwrap();
         repo.add(Box::new(BuiltinTestAssertsPlugin)).unwrap();
+        repo.add(Box::new(dojo_lang::BuiltinDojoPlugin)).unwrap();
         repo
     }
 
@@ -88,6 +91,17 @@ impl CairoPluginRepository {
     }
 
     pub fn fetch(&self, id: PackageId) -> Result<&dyn CairoPlugin> {
+        // Dojo is added as std until proc macro are fully supported.
+        let id = if id.name.to_string() == dojo_lang::DOJO_PLUGIN_PACKAGE_NAME {
+            PackageId::new(
+                PackageName::new(dojo_lang::DOJO_PLUGIN_PACKAGE_NAME),
+                id.version.clone(),
+                SourceId::for_std(),
+            )
+        } else {
+            id
+        };
+
         self.get(id)
             .ok_or_else(|| anyhow!("compiler plugin could not be loaded `{id}`"))
     }
